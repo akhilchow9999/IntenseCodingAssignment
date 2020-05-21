6681897d770abab6c2a0100589b0bab27ac6659d
akhilchow9999 <54537917+akhilchow9999@users.noreply.github.com>
2020-05-21 14:27:52 +0530

    Initial commit

---
+++ IntenseCodingQuestion1/Program.cs
@@ -0,0 +1,224 @@
+﻿/*
+ * This project is for showing wheather parking slots are available and to accpet valet parking.
+ * Developed By C Akhil Chowdary and B Hemanth on 20/05/2020 
+ */
+using System;
+
+namespace IntenseCodingQuestion1
+{
+    //Enum to show Parking Status
+    internal enum ParkingStatus
+    {
+        Empty,
+        Full
+    }
+
+    public class Slots
+    {
+        public int RegularParkingSlots;
+        public int HandicappedParkingSlots;
+        public int CompactParkingSlots;
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Slots slots = new Slots();
+            
+            //Inputting Regular Slots
+            Console.WriteLine("Please Provide no. of Regular Parking Slots");
+            bool isRegularParkingConverted = int.TryParse(Console.ReadLine(), out slots.RegularParkingSlots);
+            while (!isRegularParkingConverted)
+            {
+                Console.WriteLine("Please Enter an integer value");
+                isRegularParkingConverted = int.TryParse(Console.ReadLine(), out slots.RegularParkingSlots);
+            }
+
+            //Inputting Handicapped Slots
+            Console.WriteLine("Please Provide no. of Handicapped Parking Slots");
+            bool isHandicappedParkingConverted = int.TryParse(Console.ReadLine(), out slots.HandicappedParkingSlots);
+            while (!isHandicappedParkingConverted)
+            {
+                Console.WriteLine("Please Enter an integer value");
+                isHandicappedParkingConverted = int.TryParse(Console.ReadLine(), out slots.HandicappedParkingSlots);
+            }
+
+
+            //Inputting Compact Slots
+            Console.WriteLine("Please Provide no. of Compact Parking Slots");
+            bool isCompactParkingCOnverted = int.TryParse(Console.ReadLine(), out slots.CompactParkingSlots);
+            while (!isCompactParkingCOnverted)
+            {
+                Console.WriteLine("Please Enter an integer value");
+                isCompactParkingCOnverted = int.TryParse(Console.ReadLine(), out slots.CompactParkingSlots);
+            }
+
+
+            //Displaying No. of slots per each Parking type
+            Console.WriteLine("Regular Slots    : " + slots.RegularParkingSlots);
+            Console.WriteLine("Handicapped Slots: " + slots.HandicappedParkingSlots);
+            Console.WriteLine("Compact Slots    : " + slots.CompactParkingSlots);
+
+
+            int HandicappedParkingTracker = 0; //Tracker for Handicapped slots
+            int RegularParkingTracker = 0; //Tracker for Regular slots
+            int CompactparkingTracker = 0; //Tracker for Compact slots
+            int ClientNumber = 0; //Keeping a track for no of requests made to the program
+            string Park; //Variable to store if client's input to park or not
+            string ParkingType; //Variable to check type of parking
+
+            //infinite loop
+            while (1 > 0)
+            {
+                ClientNumber++;
+
+                //Checking if the client is handicapped or Senior Citizen
+                Console.WriteLine("Client :" + ClientNumber);
+                Console.WriteLine("Is the client a handicapped or senior citizen?(Y/N)");
+                string isHandicappedOrSeniorCitizen = Console.ReadLine();
+
+                string isValetParking; //Variable to check if user wants Valet Parking or not
+
+                //Code for Handicapped Parking
+                if (isHandicappedOrSeniorCitizen == "Y" || isHandicappedOrSeniorCitizen == "y")
+                {
+                    Console.WriteLine("He is eligible handicapped parking");
+
+                    Console.WriteLine("Do you want to park?(Y/N)");
+                    Park = Console.ReadLine();
+
+                    //If client chooses to park
+                    if (Park == "Y" || Park == "y")
+                    {
+
+                        //If the slot has empty space for parking
+                        if (HandicappedParkingTracker < slots.HandicappedParkingSlots)
+                        {
+                            HandicappedParkingTracker++;
+
+                            Console.WriteLine("Handicapped Parking is " + Enum.GetName(typeof(ParkingStatus), 0) + ", You can Park!");
+
+                            //Valet Parking 
+                            Console.WriteLine("Do you prefer valet parking?(Y/N)");
+                            isValetParking = Console.ReadLine();
+                            if (isValetParking == "Y" || isValetParking == "y")
+                            {
+                                Console.WriteLine("Thank You! We'll take care of your parking");
+                            }
+                            else
+                                Console.WriteLine("Please use the parking lot carefully");
+
+                        }
+                        else
+                            Console.WriteLine("Handicapped Parking is " + Enum.GetName(typeof(ParkingStatus), 1) + ",Please wait for some time");
+
+                    }
+
+                    //If client chooses not to park
+                    else if (Park == "N" || Park == "n")
+                        continue;
+
+                    //If Client enters wrong input for parking
+                    else
+                        Console.WriteLine("Please provide valid option for parking");
+                }
+
+                //If the client is nor Handicapped or Senior Citizen
+                else if (isHandicappedOrSeniorCitizen == "N" || isHandicappedOrSeniorCitizen == "n")
+                {
+                    Console.WriteLine("He is not eligible handicapped parking");
+                    Console.WriteLine("Do you want to park?(Y/N)");
+                    Park = Console.ReadLine();
+
+                    //If client chooses to park
+                    if (Park == "Y" || Park == "y")
+                    {
+                        //Asking for the type of parking
+                        Console.WriteLine("Which type of parking do you choose?");
+                        Console.WriteLine("1. Regular");
+                        Console.WriteLine("2. Compact");
+
+                        ParkingType = Console.ReadLine();
+                        switch (ParkingType)
+                        {
+                            //Regular Parking
+                            case "1":
+                                {
+                                    Console.WriteLine("welcome to regular parking");
+                                    if (RegularParkingTracker < slots.RegularParkingSlots)
+                                    {
+                                        RegularParkingTracker++;
+
+                                        Console.WriteLine("Regular Parking is " + Enum.GetName(typeof(ParkingStatus), 0) + ", You can park");
+                                        Console.WriteLine("Do you prefer valet parking?(Y/N)");
+                                        isValetParking = Console.ReadLine();
+                                        if (isValetParking == "Y" || isValetParking == "y")
+                                        {
+                                            Console.WriteLine("Thank You! We'll take care of your parking");
+                                        }
+                                        else
+                                            Console.WriteLine("Please use the parking lot carefully");
+
+                                    }
+                                    else
+                                        Console.WriteLine("Regular Parking is " + Enum.GetName(typeof(ParkingStatus), 1) + ", Please wait for some time");
+
+                                    continue;
+                                }
+
+                            //Compact Parking
+                            case "2":
+                                {
+                                    Console.WriteLine("welcome to compact parking");
+                                    if (CompactparkingTracker < slots.CompactParkingSlots)
+                                    {
+                                        CompactparkingTracker++;
+
+                                        Console.WriteLine("Compact Parking is " + Enum.GetName(typeof(ParkingStatus), 0) + ", You can Park");
+                                        Console.WriteLine("Do you prefer valet parking?(Y/N)");
+                                        isValetParking = Console.ReadLine();
+                                        if (isValetParking == "Y" || isValetParking == "y")
+                                        {
+                                            Console.WriteLine("Thank You! We'll take care of your parking");
+                                        }
+                                        else
+                                            Console.WriteLine("Please use the parking lot carefully");
+
+                                    }
+                                    else
+                                        Console.WriteLine("Compact Parking is " + Enum.GetName(typeof(ParkingStatus), 1) + ", Please wait for some time");
+
+                                    continue;
+                                }
+
+                            //For wrong inputs
+                            default:
+                                {
+                                    Console.WriteLine("Please enter valid type of parking");
+
+                                    continue;
+                                }
+                        }
+                    }
+
+                    //If Client chooses not to park
+                    else if (Park == "N" || Park == "n")
+                        Console.WriteLine();
+
+                    //If client provides wrong input to parking
+                    else
+                        Console.WriteLine("Please provide valid parking option");
+
+                }
+
+                //If client provides wrong input to Handicapped or Senior Citizen
+                else
+                    Console.WriteLine("Please provide valid client details");
+
+                Console.WriteLine("------------------------------------");
+            }
+        }
+    }
+}
---
+++ IntenseCodingQuestion2/RotateArrayByGivenUnit.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * This project is for taking in an input array and the unit by which
+ * it has to be rotated and diplay the corresponding rotated array.
+ * Developed By C Akhil Chowdary on 20/05/2020 
+ */
+
+using System;
+
+//namespace
+namespace IntenseCodingQuestion2
+{
+    //OUr Class
+    class RotateArrayByGivenUnit
+    {
+        static void Main(string[] args)
+        {
+            int k, n; //Length of array and the Unit by which it has to be rotated.
+           
+            //Infinite loop
+            while (true)
+            {
+                //Taking the length of Array
+                Console.WriteLine("Enter the length of Array");
+                bool isNInteger = int.TryParse(Console.ReadLine(), out n);
+                while(!isNInteger||n<1)
+                {
+                    Console.WriteLine("Please Enter Natural Numbers only");
+                    isNInteger = int.TryParse(Console.ReadLine(), out n);
+                }
+
+                //Declaring Input and Output Arrays
+                int[] InputArray = new int[n];
+                int[] RotatedArray = new int[n];
+
+                //Taking Input Array
+                Console.WriteLine("Enter the input array");
+                try
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        InputArray[i] = int.Parse(Console.ReadLine());
+                    }
+                }
+                //Catch block for exceptions occurred in Reading Input Array
+                catch (Exception)
+                {
+                    Console.WriteLine("Enter Integers Only");
+                    continue;
+                }
+              
+                //Taking the unit by which Array has to be rotated
+                Console.WriteLine("Enter the value by which you want to rotate the Array");
+                bool isKInteger = int.TryParse(Console.ReadLine(), out k);
+                while (!isKInteger )
+                {
+                    Console.WriteLine("Please Enter Natural Numbers only");
+                    isKInteger = int.TryParse(Console.ReadLine(), out k);
+                }
+
+                //Conditional check-1 for the Unit by which Array has to be rotated: it must be made less than the length of array
+                if (k >= n)
+                    k %= n;
+
+                //Conditional check-2 for the Unit by which Array has to be rotated it must be a positive integer
+                while (k < 0)
+                    k += n;
+
+                //Storing the resulting array in output array
+                for (int i = 0; i < n; i++)
+                {
+                    RotatedArray[(i + k) % n] = InputArray[i];
+                }
+
+                //Displaying output
+                Console.WriteLine("The Rotated Array:", k);
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(RotatedArray[i] + " ");
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
